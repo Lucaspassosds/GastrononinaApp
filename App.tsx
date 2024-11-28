7fa16f97754e4d96718263cda6b5dd2d8d48490e
lucas <lucas@readyon.ai>
2024-11-28 13:33:52 -0300

    feat : added fonts

--- App.tsx
+++ App.tsx
@@ -1,14 +1,38 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import * as SQLite from "expo-sqlite";
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import "./global.css";
+import { useEffect, useState } from "react";
+import * as Font from "expo-font";
 
 const expo = SQLite.openDatabaseSync("db.db");
 
 const db = drizzle(expo);
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      Birdgo: require("./assets/fonts/Birdgo.ttf"),
+      "Chopin-Trial": require("./assets/fonts/Chopin-Trial.ttf"),
+    });
+    setFontsLoaded(true);
+  };
+
+  useEffect(() => {
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
